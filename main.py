33adde799a4601908fd1fd981830e97ba783c63e
ManojSharma <sharma.manoj84@yahoo.co.in>
2025-02-21 16:38:19 +0530

    Added lines at each 10 mm

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ for idx, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    for y in range(20, 298, 10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y)
+
     pdf.ln(265)   # adding 278 break lines
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
@@ -26,5 +28,7 @@ for idx, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
 
 pdf.output("output.pdf")
